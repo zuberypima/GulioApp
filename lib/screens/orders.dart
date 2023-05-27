e36863091e02c0a55a2f13d1ee4545f2648ec327
zuberypim <zuberypima12@gmail.com>
2023-05-27 15:17:17 +0300

    update messages page

--- lib/screens/orders.dart
+++ lib/screens/orders.dart
@@ -11,49 +11,57 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
-
   @override
   Widget build(BuildContext context) {
-       final Stream<QuerySnapshot> _odersStream = FirebaseFirestore.instance
+    final Stream<QuerySnapshot> _odersStream = FirebaseFirestore.instance
         .collection('Users')
         .doc(user!.email)
-        .collection('Messages')
+        .collection('OrderPres')
         .snapshots();
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: ConstantsColors().mainColor(),
-        title: Text('Orders'),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-      stream: _odersStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
-        }
-
-        return ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-            return  Card(
-              child: ListTile(
-               minVerticalPadding: 5.0,
-                title: Text('Juma Shabani'),
-                subtitle: Row(children: [Text('Bei'),SizedBox(width: 10,),Text('10000')],),
-                trailing: Icon(Icons.forward),
-              ),
+        appBar: AppBar(
+          backgroundColor: ConstantsColors().mainColor(),
+          title: Text('Orders'),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: _odersStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                return Card(
+                  child: ListTile(
+                    minVerticalPadding: 5.0,
+                    title: Text(data['Buyer']),
+                    subtitle: Row(
+                      children: [
+                        Text('Bei'),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(data['ofa'])
+                      ],
+                    ),
+                    trailing: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => Messages()));
+                        },
+                        child: Icon(Icons.forward)),
+                  ),
+                );
+              }).toList(),
             );
-          }).toList(),
-        );
-      },
-    )
-    );
+          },
+        ));
   }
 }
-
-
-
